b8d1d9d28476929eb8c853060dd13b6bae4a0774
Brenda Marmolejo <bmarmolejo@gmail.com>
2024-08-11 22:52:11 -0400

    completed hero and condo description

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,32 @@
+import Hero from "../components/Hero/Hero";
+import Sebastian from "../components/Sebastian/Sebastian";
+
+// import Gallery from "../components/Gallery/Galery";
+// import BookCar from "../components/BookCar";
+// import PlanTrip from "../components/PlanTrip";
+// import PickCar from "../components/PickCar";
+// import Banner from "../components/Banner";
+// import ChooseUs from "../components/ChooseUs";
+// import Testimonials from "../components/Testimonials";
+// import Faq from "../components/Faq";
+// import Download from "../components/Download";
+// import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Sebastian />
+      {/* <BookCar />
+      <PickCar />
+      <Banner />
+      <ChooseUs />
+      <Testimonials />
+      <Faq />
+      <Download />
+      <Footer /> */}
+    </>
+  );
+}
+
+export default Home;
