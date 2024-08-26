46b04cce497ef1681d802f88cec88fe632d62c9f
Brenda Marmolejo <bmarmolejo@gmail.com>
2024-08-26 01:48:19 -0400

    updated learn more button

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+import "./Banner.scss";
+function Banner() {
+    return (
+      <>
+        <section className="banner-section">
+          <div className="container">
+            <div className="banner-content">
+              <div className="banner-content__text">
+                <h2>Your Mazatlán Escape Awaits</h2>
+                <p>
+                Reserve Your Spot at Mazatlán's Newest Retreat Before It Debuts! <span>Special Rates</span> for friends and family!
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
+  
+  export default Banner;
