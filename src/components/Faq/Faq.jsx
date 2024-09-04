ff6b450550509f90e3e92c9cb5fd3f385e96d06d
Brenda Marmolejo <bmarmolejo@gmail.com>
2024-09-04 13:53:47 -0400

    corrected hero for mobile view

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -25,8 +25,8 @@ function Faq() {
               <h5>FAQ</h5>
               <h2>Frequently Asked Questions</h2>
               <p>
-                Frequently asked questions about the rental booking process
-                on our website: Answers to common concerns and inquiries.
+                Frequently asked questions about the rental booking process on
+                our website: Answers to common concerns and inquiries.
               </p>
             </div>
 
@@ -45,7 +45,11 @@ function Faq() {
                   onClick={() => openQ("q1")}
                   className={`faq-box__answer ${getClassAnswer("q1")}`}
                 >
-To confirm your reservation and secure your dates, we require payment in advance. Once the payment is received, we will finalize all preparations for your stay, ensuring a comfortable experience in Mazatlán.</div>
+                  To confirm your reservation and secure your dates, we require
+                  payment in advance. Once the payment is received, we will
+                  finalize all preparations for your stay, ensuring a
+                  comfortable experience in Mazatlán.
+                </div>
               </div>
               <div className="faq-box">
                 <div
@@ -53,7 +57,9 @@ To confirm your reservation and secure your dates, we require payment in advance
                   onClick={() => openQ("q2")}
                   className={`faq-box__question ${getClassQuestion("q2")}`}
                 >
-                  <p>2. What features and comforts are included in the condo?</p>
+                  <p>
+                    2. What features and comforts are included in the condo?
+                  </p>
                   <i className="fa-solid fa-angle-down"></i>
                 </div>
                 <div
@@ -61,7 +67,13 @@ To confirm your reservation and secure your dates, we require payment in advance
                   onClick={() => openQ("q2")}
                   className={`faq-box__answer ${getClassAnswer("q2")}`}
                 >
-Our condo is designed with your comfort in mind, offering everything you need for a relaxing stay. You'll enjoy air conditioning, smart TVs, high-speed internet, a cozy bedroom with a queen-size bed, an additional bedroom with twin beds, and a sofa bed in the living room. Our goal is to make the space stylish, welcoming, and perfectly suited for your needs.                </div>
+                  Our condo is designed with your comfort in mind, offering
+                  everything you need for a relaxing stay. You'll enjoy air
+                  conditioning, smart TVs, high-speed internet, a cozy bedroom
+                  with a queen-size bed, an additional bedroom with twin beds,
+                  and a sofa bed in the living room. Our goal is to make the
+                  space stylish, welcoming, and perfectly suited for your needs.{" "}
+                </div>
               </div>
               <div className="faq-box">
                 <div
@@ -77,7 +89,12 @@ Our condo is designed with your comfort in mind, offering everything you need fo
                   onClick={() => openQ("q3")}
                   className={`faq-box__answer ${getClassAnswer("q3")}`}
                 >
-                  The apartment is conveniently located just 250 meters from the beach, placing you within a short stroll of Mazatlán's stunning shoreline. You'll also be near the vibrant Malecón, a lively area filled with restaurants, shops, and cultural attractions. It's the perfect spot to experience the best of what Mazatlán has to offer.
+                  The apartment is conveniently located just 250 meters from the
+                  beach, placing you within a short stroll of Mazatlán's
+                  stunning shoreline. You'll also be near the vibrant Malecón, a
+                  lively area filled with restaurants, shops, and cultural
+                  attractions. It's the perfect spot to experience the best of
+                  what Mazatlán has to offer.
                 </div>
               </div>
             </div>
